df44c1447a5b0c86b118920c29cfc966f7704e73
NatiaLobzhanidze <nati.lobzhanidze@gmail.com>
2022-04-06 18:33:12 +0400

    finish visual editing

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -5,15 +5,33 @@
 //  Created by Natia's Mac on 06/04/2022.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mySwitch: UISwitch!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var historyLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
-
+    @IBAction func changeBack(_ sender: UISwitch) {
+        if sender.isOn{
+            view.backgroundColor = .black
+            mySwitch.thumbTintColor = .black
+            resultLabel.textColor = .white
+            historyLabel.textColor = .white
+        } else {
+            let peri = UIColor(rgb: 0xF1F2F3)
+            view.backgroundColor = peri
+            mySwitch.thumbTintColor = .white
+            resultLabel.textColor = .black
+            historyLabel.textColor = .black
+        }
+    }
+    
 }
 
